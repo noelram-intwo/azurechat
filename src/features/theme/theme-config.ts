a860712c7867432605ede13a6b92f22c0637fd7d
noelram-intwo <157612832+noelram-intwo@users.noreply.github.com>
2024-08-21 16:44:34 -0400

    Update chatbot description

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
 export const AI_NAME = "Intwo OpenaAI Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_DESCRIPTION = "This chatbot is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
